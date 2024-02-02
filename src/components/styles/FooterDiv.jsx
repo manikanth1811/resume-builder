f8d7cdd00eecdcc8e081fc195e6cad79767410a6
Manikanth <42494837+manikanth1811@users.noreply.github.com>
2024-02-02 16:47:55 -0600

    First Commit

---
+++ src/components/styles/FooterDiv.jsx
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+export const FooterDiv = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  min-height: fit-content;
+  width: 100%;
+  max-height: 40px;
+  background-color: royalblue;
+`;
